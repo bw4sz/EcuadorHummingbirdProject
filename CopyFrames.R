7e7ae42d686a9213db7988300e49331f972f9b0c
bw4sz <benweinstein2010@gmail.com>
2019-05-06 12:27:43 -0700

    push for rafi

--- CopyFrames.R
+++ CopyFrames.R
@@ -1,36 +1,36 @@
-##look for completed observations, move the files
-library(stringr)
-library(dplyr)
-
-basename<-c("/Users/Ben/Dropbox/HummingbirdProject/Data")
-outputpath<-c("/Users/Ben/Dropbox/HummingbirdProject/Completed_Frames")
-
-#list dirs
-dirs<-list.dirs(basename,recursive = F,full.names = F)
-
-dirs<-dirs[!dirs=="HummingbirdProjectCleaned"]
-
-for( i in 1:length(dirs)){
-  x<-dirs[i]
-  full_path<-paste(basename,x,"foundframes",sep="/")
-  out_folder<-paste(outputpath,x,"foundframes",sep="/")
+  ##look for completed observations, move the files
+  library(stringr)
+  library(dplyr)
   
-  #call to system
-  system(paste("ditto",full_path,out_folder,sep=" "))
+  basename<-c("/Users/Ben/Dropbox/HummingbirdProject/Data")
+  outputpath<-c("/Users/Ben/Dropbox/HummingbirdProject/Completed_Frames")
   
-  #find camera directories
-  from<-list.dirs(full_path,recursive = T,full.names = T)
+  #list dirs
+  dirs<-list.dirs(basename,recursive = F,full.names = F)
   
-  #it lists itself as the first entry
-  from<-from[-1]
+  dirs<-dirs[!dirs=="HummingbirdProjectCleaned"]
   
-  if (length(from)==0){
-    next
-  }
-  
-  #remove directories
-  for(y in from){
-    unlink(y,recursive=T)
-  }
-  
-  }
+  for( i in 1:length(dirs)){
+    x<-dirs[i]
+    full_path<-paste(basename,x,"foundframes",sep="/")
+    out_folder<-paste(outputpath,x,"foundframes",sep="/")
+    
+    #call to system
+    system(paste("ditto",full_path,out_folder,sep=" "))
+    
+    #find camera directories
+    from<-list.dirs(full_path,recursive = T,full.names = T)
+    
+    #it lists itself as the first entry
+    from<-from[-1]
+    
+    if (length(from)==0){
+      next
+    }
+    
+    #remove directories
+    for(y in from){
+      unlink(y,recursive=T)
+    }
+    
+    }
